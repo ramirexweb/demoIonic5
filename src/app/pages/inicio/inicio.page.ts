2345b688a24cf6404525522af257de5c2e2d1170
ramirex <ramirex@gmail.com>
2020-02-21 16:38:51 -0400

    ion-grid...

--- src/app/pages/inicio/inicio.page.ts
+++ src/app/pages/inicio/inicio.page.ts
@@ -47,6 +47,11 @@ export class InicioPage implements OnInit {
       icon: 'car',
       name: 'Fabs',
       redirecTo: '/fab'
+    },
+    {
+      icon: 'grid',
+      name: 'Grid - Rows',
+      redirecTo: '/grid'
     }
   ];
 
