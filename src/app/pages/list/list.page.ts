0f8f550517ff74b54028cdfe86a6042cde1fc3ee
ramirex <ramirex@gmail.com>
2020-02-26 19:43:36 -0400

    ion-toast

--- src/app/pages/list/list.page.ts
+++ src/app/pages/list/list.page.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { DataService } from '../../services/data.service';
 import { Observable } from 'rxjs';
-import { IonList } from '@ionic/angular';
+import { IonList, ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-list',
@@ -15,7 +15,8 @@ export class ListPage implements OnInit {
   usuarios: Observable<any>;
 
   constructor(
-    private dataService: DataService
+    private dataService: DataService,
+    private toastController: ToastController
   ) { }
 
   ngOnInit() {
@@ -23,16 +24,28 @@ export class ListPage implements OnInit {
     this.usuarios = this.dataService.getUsers();
   }
 
+  async presentToast(message: string) {
+    const toast = await this.toastController.create({
+      message,
+      duration: 2000
+    });
+
+    toast.present();
+  }
+
   favorite(user: any){
-    console.log('favorite', user);
+    // console.log('favorite', user);
+    this.presentToast('Favorite');
     this.lista.closeSlidingItems();
   }
   share(user: any){
-    console.log('share', user);
+    // console.log('share', user);
+    this.presentToast('Compartido!!');
     this.lista.closeSlidingItems();
   }
   unread(user: any){
-   console.log('unread', user);
+   // console.log('unread', user);
+   this.presentToast('Eliminado!!');
    this.lista.closeSlidingItems();
   }
 }
